c4b79e0fa1dc142fc81c730a849bc054f3000fc1
fukumomoo <132264713+fukumomoo@users.noreply.github.com>
2023-05-01 21:26:46 +0900

    practice

---
+++ CS01.cs
@@ -0,0 +1,9 @@
+public partial class C01{
+private void practice (){
+    var number = 1;
+    console.log(numbedr);
+}
+practice();
+
+}
+
---
+++ UI/Properties.cs
@@ -0,0 +1,6 @@
+using System;
+namespace  practice {
+    class C{
+
+    }
+}
